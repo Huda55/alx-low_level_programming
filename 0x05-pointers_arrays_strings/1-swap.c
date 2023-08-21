e5e11a9856ca9bc1ea6d6107099ab50d5550b974
Huda55 <hudarabie55@gmail.com>
2023-08-21 08:39:50 -0700

    new commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
-*reset_to_98 - reset value of an integer to 98
-*@n: parameter targeted
-*Return : nothing
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
 */
 
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 {
-*n = 98;
+int tmp = *a;
+*a = *b;
+*b = tmp;
 }
